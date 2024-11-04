5347e617529fc4072e1b66c9ff787ae186c4040c
Lucas Kaine <lkaine24@gmail.com>
2024-11-04 13:57:24 -0800

    B: function to get word count

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def read_book(path):
+    # Open file and assign contents to file_contents
     with open(path) as f:
         file_contents = f.read()
         
-        print(file_contents)
+        return file_contents
+
+def get_word_count(words):
+    # Split words into list, and obtain the number of indicies in that list to get the word count
+    wc = len(words.split())
+    return wc
 
 def main():
-    read_book("books/frankenstein.txt")
+    words = read_book("books/frankenstein.txt")
+    print(get_word_count(words))
 
 if __name__ == '__main__':
     main()
